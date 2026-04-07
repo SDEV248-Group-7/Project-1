242f03501cc3a5cc36156348be27cf00e2b38227
Enter-Name <Enter-Name@outlook.com>
2026-04-07 17:03:29 -0400

    Mass creation of scenes
    Made a bunch of empty scenes for entities so I can call them from room_base

---
+++ group-7-project-1/scenes/entities/small/small_ghost.gd
@@ -0,0 +1,15 @@
+extends StaticBody2D
+
+# Should move away from player, bound by enviroment
+# Low health,
+# Does damage to player if touched
+# dealth damage to it if hit by player
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- group-7-project-1/scenes/rooms/room_base/spawning_entities.gd
+++ group-7-project-1/scenes/rooms/room_base/spawning_entities.gd
@@ -1,6 +1,13 @@
 extends TileMapLayer
 
-var TREE : SceneTree = get_tree();
+## This is the room the player is in.
+var ROOM : Node2D = get_parent();
+
+# Prepared scenes for spawning
+@onready var small_ghost_scene : Resource = load("res://scenes/entities/small/small_ghost.tscn");
+@onready var large_ghost_scene : Resource = load("res://scenes/entities/large/large_ghost.tscn");
+@onready var key_scene : Resource = load("res://scenes/entities/key/key.tscn");
+@onready var npc_scene : Resource = load("res://scenes/entities/npc/npc.tscn");
 
 # This ready function should spawn in each entity by type
 # Options are...
@@ -9,7 +16,7 @@ var TREE : SceneTree = get_tree();
 	# key
 	# npc (Resident)
 func _ready() -> void:
-	pass # Replace with function body.
+	pass;
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
