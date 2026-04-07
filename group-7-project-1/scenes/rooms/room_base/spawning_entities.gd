f5e330d354c060893025d00422fc6136f75866c8
Enter-Name <Enter-Name@outlook.com>
2026-04-07 14:12:47 -0400

    Tilemap Layers 01
    Added Environment and Entity layers and started building out the scripts for this scene

---
+++ group-7-project-1/scenes/rooms/room_base/spawning_entities.gd
@@ -0,0 +1,17 @@
+extends TileMapLayer
+
+var TREE : SceneTree = get_tree();
+
+# This ready function should spawn in each entity by type
+# Options are...
+	# Small Ghost
+	# Large Ghost
+	# Key
+	# Resident
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
