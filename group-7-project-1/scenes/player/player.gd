bfbf544b35dddf2bcd4adac1484041144f94351a
Enter-Name <Enter-Name@outlook.com>
2026-04-12 21:35:47 -0400

    Fixed Moving

--- group-7-project-1/scenes/player/player.gd
+++ group-7-project-1/scenes/player/player.gd
@@ -32,8 +32,8 @@ func _physics_process(delta):
 
 
 func handle_movement():
-	direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	direction.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 
 	if direction != Vector2.ZERO:
 		direction = direction.normalized()
